de94687ed09c306afea6c684e6f9c6674a64897b
student <student@DESKTOP-4ARRLB2>
2022-09-03 11:20:40 +0800

    11:20

--- mvn_spring0903/src/main/java/demo/springmvc/HelloWorldController.java
+++ mvn_spring0903/src/main/java/demo/springmvc/HelloWorldController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//this is a class
 @Controller
 public class HelloWorldController {
  
