78877bf71013501d1d9f6854a492f8d6225bae28
student <student@DESKTOP-4ARRLB2>
2022-09-03 15:16:10 +0800

    03:16

---
+++ mvn_spring0903/src/main/java/demo/springmvc/StudentController.java
@@ -0,0 +1,30 @@
+package demo.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import demo.model.Student;
+
+@Controller
+public class StudentController {
+
+@RequestMapping(value = "/student" , method = RequestMethod.GET)
+	public ModelAndView student(@ModelAttribute("student") Student student)
+	{ 
+	     Student s=new Student(20,"Mary",7);
+	     return new ModelAndView("student", "command", s); 
+	}
+
+	@RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+	public String addStudent(@ModelAttribute("student") Student student, ModelMap model) {
+		model.addAttribute("name", student.getName());
+		model.addAttribute("age", student.getAge());
+		model.addAttribute("id", student.getId());
+
+		return "result";
+	}
+}
