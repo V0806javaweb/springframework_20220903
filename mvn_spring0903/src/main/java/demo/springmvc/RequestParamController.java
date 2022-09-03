a85469ef4e8d3ceb48e3fb9bd47ee74b83adda0b
student <student@DESKTOP-4ARRLB2>
2022-09-03 11:49:19 +0800

    mvc example

--- mvn_spring0903/src/main/java/demo/springmvc/RequestParamController.java
+++ mvn_spring0903/src/main/java/demo/springmvc/RequestParamController.java
@@ -3,14 +3,15 @@ package demo.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RequestParamController {
      
-    @RequestMapping("/user")
-    public String userInfo(Model model, @RequestParam(value = "name", defaultValue = "Guest") String name) {
- 
+    @RequestMapping(value="/user", method = RequestMethod.POST)
+    public String userInfo(Model model, @RequestParam(value = "name", defaultValue = "Guest") String name ) {
+    	 
         model.addAttribute("name", name);
  
         if("admin".equals(name)) {
@@ -19,6 +20,12 @@ public class RequestParamController {
             model.addAttribute("email", "Not set");
         }
         return "userInfo";
+    }
+  
+    @RequestMapping("/userui")
+    public String userInfo(Model model) {
+    	model.addAttribute("username", "admin");
+        return "userform";
     }
   
 }
